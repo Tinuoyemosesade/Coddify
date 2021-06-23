fccd54fdd66e8b222fc5b03e0c86f6c847c34057
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-06-23 18:53:52 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,40 +1,41 @@
-class student{
+class Student{
+
     constructor(){
-        this.name;
-        this.age;
-        this.marks;
+        this.name ;
+        this.age ;
+        this.marks ;
     }
+
+    // getter
+    getName(){
+        return this.name;
+    }
+    getAge(){
+        return this.age;
+    }
+    getMarks(){
+        return this.marks;
+    }
+
+    // setter
+    setName(name){
+        this.name = name;
+    }
+
+    setAge(age){
+        this.age = age;
+    }
+
+    setMarks(marks){
+        this.marks = marks;
+    }
+
 }
 
-//getter
-getName(){
-    return this.name;
-}
+//"Jane",30,[22,50,60,70]
 
-getAge(){
-    return this.age;
-}
-
-getMarks(){
-    return this.marks;
-}
-
-//setter
-setName(name)(
-    this.name = name;
-)
-
-setAge(age){
-    this.age = age;
-}
-
-setMarks(marks){
-    this.marks = marks;
-}
-
-
-let student = new student()
+let student1 = new Student()
 
 student1.setName('jane')
 student1.setAge(30)
-student.setMarks([22,50,60,70]);
+student1.setMarks([22,50,60,70])
