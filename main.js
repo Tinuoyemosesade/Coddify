5899947d940d7d44a2244fe1fd15aa754308f243
Moses Tinuoye <63667580+Tinuoyemosesade@users.noreply.github.com>
2021-06-23 18:35:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+/*
+//spread operator
+function addNumbers(a, b, c){
+    return a + b + c
+}
+
+let numbers =[5, 12, 8];
+
+document.write(addNumbers.apply(null, numbers));
+
+document.write(addNumbers(...numbers */
+
+
+
+
+
+
+
+let greet = "Hello world";
+const PI = 3.14;
+function multiplyNumbers(a,b){
+    return a*b;
+}// exporting variables and functions
+
+export { greet, PI, multiplyNumbers } 
+
